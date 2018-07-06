240d17113a5d5cab369043c376dc5913e9b473b9
Flo <s0565635@htw-berlin.de>
2018-07-06 18:20:43 +0200

    playing around with sanitycheck...

--- src/de/sb/tournament/persistence/SanityCheck.java
+++ src/de/sb/tournament/persistence/SanityCheck.java
@@ -12,9 +12,18 @@ import javax.persistence.Query;
 public class SanityCheck {
 
 	public static void main (final String[] args) {
+		//TODO: DO sanityCheck
 		final EntityManagerFactory emf = Persistence.createEntityManagerFactory("tournament");
 		final EntityManager em = emf.createEntityManager();
-		final Query query = em.createQuery("select t from Tournament as t");
-		System.out.println(query.getResultList());
+		
+		final Query query1 = em.createQuery("select t from Tournament as t");
+		System.out.println(query1.getResultList());
+		
+		
+		final Query query2 = em.createQuery("select t from TournamentType as t");
+		System.out.println(query2.getResultList());
+		
+		final Query query3 = em.createQuery("select m from Match as m");
+		System.out.println(query3.getResultList());
 	}
 }
